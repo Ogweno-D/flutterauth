e30d74fb4832f5a0629f0d1647eef0592a6a08c3
Ogweno-D <dennisogwene@gmail.com>
2025-01-29 10:58:37 +0300

    Enhance HomePage UI

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,7 +4,7 @@ import 'package:flutterauth/components/my_drawer.dart';
 import 'package:flutterauth/services/auth_service.dart';
 
 class HomePage extends StatelessWidget {
-   HomePage({super.key});
+  HomePage({super.key});
 
   final User? currentUser = AuthService().currentUser!;
 
@@ -12,16 +12,32 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Center(
+            child: Text(
+              "",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+        elevation: 0,
         actions: [
           IconButton(
             icon: Icon(Icons.logout),
-            onPressed: ()=>AuthService().signOut(),
+            onPressed: () => AuthService().signOut(),
           )
         ],
       ),
       drawer: MyDrawer(),
       body: Center(
-        child: Text("Welcome  ${currentUser!.email!}"),
+        child: Text(
+          "Welcome  ${currentUser!.email!}",
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
     );
   }
