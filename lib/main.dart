4978428e58c8b6d6c960b62ccec1a469308c2415
Ogweno-D <dennisogwene@gmail.com>
2025-01-29 10:42:32 +0300

    Remove unused import for home_page in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutterauth/pages/auth_page.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutterauth/pages/home_page.dart';
 import 'firebase_options.dart';
 
 
